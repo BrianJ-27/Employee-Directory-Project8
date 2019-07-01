82265c3800f2f1c6ed142449869f957d2fca572c
Brian <johnson8727@hotmail.com>
2019-07-01 17:11:14 -0400

    updating code with comments about how my program flows

--- js/main.js
+++ js/main.js
@@ -48,9 +48,8 @@ function fetchData(url){
   // ------------------------------------------
 
 
-
-  function displayEmployees (employeeData ) {
-    
+//This function adds the employees onto the page
+  function displayEmployees (employeeData) {
    // storing an empty string into a variable so we can dynamically interpolate the HTML to it
    let employeeHTML = " ";
    //We are itering or looping over the 12 employees data array, 
@@ -80,10 +79,12 @@ function fetchData(url){
    
 }
 
-
+// This function call is located on 3 different listeners
+// 1) grid container listener invokes this function when the user click any employee card on the page
+// 2) 
 function showModal(index){
+//
    employee = filteredEmployees[index]; 
-
   let name = employee.name;
   let dob = employee.dob;
   let phone = employee.phone;
@@ -94,7 +95,7 @@ function showModal(index){
   let postcode = employee.location.postcode;
   let picture = employee.picture.large;
   let date = new Date(dob.date);
-  
+  //created a variable to hold the HTML & string interpolated content that will show appear after a card is clicked
   const modalHTML = ` 
   <img class="modal-avatar" src="${picture}" />
   <div class="modal-text-container">
@@ -106,7 +107,9 @@ function showModal(index){
       <p class="address">${street} ${state}, ${postcode}</p>
       <p class= "bday">Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     `
+    //Remove the hidden class on the this selector to reveal the dark transparent overlay
     overlay.classList.remove("hidden");
+    //Then assign the value of modalHTML so the all the HTML markup gets added to the DOM after user clicks  
     modal.innerHTML = modalHTML;
     
 }; 
@@ -114,19 +117,27 @@ function showModal(index){
     
 
 function filterEmployee(){
+  //places the "employee" JSON data received from the server and stores the data into a the "filteredEmployees" varaible 
+  //This filteredEmployees will now hold the data in a array
     filteredEmployees = employees;
   //create a variable to hold the value of the search field and change the input to lowercase
     let inputField = document.getElementById('search-field').value.toLowerCase();
+  // checks to see if input value and arraylength are true. If true, it filters out the first names or last names
+  // of what is typed
     if (inputField && inputField.length){
-     filteredEmployees = filteredEmployees.filter((employee)=> employee.name.first.indexOf(inputField) > -1 || employee.name.last.indexOf(inputField) > -1);
+     filteredEmployees = filteredEmployees.filter((employee)=> 
+     employee.name.first.indexOf(inputField) > -1 || employee.name.last.indexOf(inputField) > -1);
     }
+  // If the "if statement" evaluates to false, then invoke the 
+  //displayEmployees function and pass the stored JSON data to begin execution of the displayEmployees function 
+  //To display this employee data to the page.
      displayEmployees(filteredEmployees); 
   }
-
+// get the h2 data from modal card and returns the value to be used later in both arrow click event listeners
   function getModalCard(){
     return document.getElementById("modalCard");
   }
-
+// gets the current index of the modal and returns the value to be used later in both arrow click event listeners
   function getCurrentIndex(modalCard){
     return modalCard.getAttribute('data-index');
   }
@@ -134,7 +145,8 @@ function filterEmployee(){
   // ------------------------------------------
   //  EVENT LISTENERS
   // ------------------------------------------
-
+// when user clicks on an employee card, it grabs the the closet employee card and grads the number value from the
+// The listener also passes a the indexed number value to the showModal function
   gridContainer.addEventListener("click", (event) =>{
     if(event.target !== gridContainer){
       const card = event.target.closest(".card");
@@ -142,7 +154,7 @@ function filterEmployee(){
       showModal(index);
     }
   });
-
+// when user clicks on left arrow it goes to the prev indexed person in the employee list and loops back to the start
   leftArrow.addEventListener("click", (event)=>{
     let card = getModalCard();
     let currentIndex = +getCurrentIndex(card);
@@ -153,7 +165,7 @@ function filterEmployee(){
      card.setAttribute('data-index', currentIndex)
      showModal(currentIndex);
   });
-
+// when user clicks on right arrow it goes to the next indexed person in the employee list and starts back around again
   rightArrow.addEventListener("click", (event)=>{
     let card = getModalCard();
     let currentIndex = +getCurrentIndex(card);
@@ -164,15 +176,13 @@ function filterEmployee(){
      card.setAttribute('data-index', currentIndex)
      showModal(currentIndex);
   });
-
+// when user clicks the close button it hides the modal/closes the modal compeletly
   closeModal.addEventListener("click", ()=>{
     overlay.classList.add("hidden");
   });
-
-  
-
+//When user keys in a value (letter) it filters through the employees and display the employees 
+//that have with that value in their first & last name
   const searchBar = document.getElementById("searchBar");
-  
   searchBar.addEventListener("keyup", filterEmployee);
   
 
