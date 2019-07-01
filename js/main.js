02f0030db24979681fbc24201807005df3249062
Brian <johnson8727@hotmail.com>
2019-07-01 18:58:34 -0400

    added a cursor pointer on the arrows and added more comments to JS code

--- js/main.js
+++ js/main.js
@@ -1,30 +1,38 @@
-// ------------------------------------------
+// --------------------------------------------------------------------//
 //  GLOBAL VARIABLES
-// ------------------------------------------
+// --------------------------------------------------------------------//
 
 // Stores the URL of the randomUser API & the options connected to the API
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US`
+
 // Stores the main element with is the main-container of all the employees
 const gridContainer = document.querySelector(".grid-container");
+
 // Stores the div element that acts as the dark overlay for the modal
 const overlay = document.querySelector(".overlay");
+
 // Stores the div element that hold the modal information
 const modal = document.querySelector(".modal-info");
+
 // Stores the the div element that is the modal's close button
 const closeModal = document.querySelector(".modal-close");
 
+// Stores the a element that is the left arrow
 const leftArrow = document.getElementById("leftArrow");
+
+// Stores the a element that is the right arrow
 const rightArrow = document.getElementById("rightArrow");
 
+// Stores an empty array that will hold the filtered employee values from the API
 let filteredEmployees = [];
 
 // Stores an empty array that will hold the values from the API
 let employees = [];
 
 
-// ------------------------------------------
+// --------------------------------------------------------------------//
 //  FETCH FUNCTIONS
-// ------------------------------------------
+// --------------------------------------------------------------------//
 
 function fetchData(url){
     return fetch(url)
@@ -43,18 +51,20 @@ function fetchData(url){
    
 
     
-  // ------------------------------------------
-  //  HELPER FUNCTIONS
-  // ------------------------------------------
+// --------------------------------------------------------------------//
+//  HELPER FUNCTIONS
+// --------------------------------------------------------------------//
 
 
 //This function adds the employees onto the page
   function displayEmployees (employeeData) {
    // storing an empty string into a variable so we can dynamically interpolate the HTML to it
    let employeeHTML = " ";
-   //We are itering or looping over the 12 employees data array, 
-   //using dot.notation to access the values inside the array and assign it to a variable
-   //Then create the HTML tags and use string interpolation to access the and set the values to an attribute
+
+   /*We are itering or looping over the 12 employees data array, using dot.notation to access 
+     the values inside the array and assign it to a variable & then create the HTML tags and 
+     use string interpolation to access the and set the values to an attribute
+   */
    employeeData.forEach((employee, index) =>{
         let name = employee.name;
         let email = employee.email;
@@ -72,18 +82,23 @@ function fetchData(url){
          `;
    }
    ); 
-   //Then we are taking our new dynamic content and assigning to gridContainer variable
-   //Then we are adding the .innerHTML() to the grid container variable to add it dynamically to the page!! :)
-   // We then call this function in our then() so the code inside this function can execute
+
+   /*Then we are taking our new dynamic content and assigning to gridContainer variable.
+    Then we are adding the .innerHTML() to the grid container variable to add it dynamically to the page !! :)
+    We then call this function in our then() so the code inside this function can execute */
    gridContainer.innerHTML = employeeHTML;
    
 }
 
-// This function call is located on 3 different listeners
+// This function call is located on 3 different listeners which has an index parameter 
 // 1) grid container listener invokes this function when the user click any employee card on the page
-// 2) 
+/* 2) The left & right arrow click listeners calls this function when a user clicks on the arrows to
+     go back & forth to show the previous & next modal employee card.
+   */
 function showModal(index){
-//
+/*takes the filtered JSON data and sets it to the the local employee variable. Then, I created 
+  multiple variables to hold the values of the JSON data using dot notation.
+   */
    employee = filteredEmployees[index]; 
   let name = employee.name;
   let dob = employee.dob;
@@ -95,6 +110,7 @@ function showModal(index){
   let postcode = employee.location.postcode;
   let picture = employee.picture.large;
   let date = new Date(dob.date);
+
   //created a variable to hold the HTML & string interpolated content that will show appear after a card is clicked
   const modalHTML = ` 
   <img class="modal-avatar" src="${picture}" />
@@ -109,44 +125,54 @@ function showModal(index){
     `
     //Remove the hidden class on the this selector to reveal the dark transparent overlay
     overlay.classList.remove("hidden");
+
     //Then assign the value of modalHTML so the all the HTML markup gets added to the DOM after user clicks  
     modal.innerHTML = modalHTML;
     
 }; 
-
-    
-
+   
+/*This function is passed into the last then() method to execute the code below that will filter the empolyee
+data and display to the page */
 function filterEmployee(){
-  //places the "employee" JSON data received from the server and stores the data into a the "filteredEmployees" varaible 
-  //This filteredEmployees will now hold the data in a array
+  /*places the "employee" JSON data received from the server and stores the data into a the 
+    "filteredEmployees" varaible This filteredEmployees will now hold the data in a array*/
     filteredEmployees = employees;
+
   //create a variable to hold the value of the search field and change the input to lowercase
     let inputField = document.getElementById('search-field').value.toLowerCase();
-  // checks to see if input value and arraylength are true. If true, it filters out the first names or last names
-  // of what is typed
+
+  /* checks to see if input value and arraylength are true. If true, it filters out the
+     first names or last namesof what is typed */
     if (inputField && inputField.length){
      filteredEmployees = filteredEmployees.filter((employee)=> 
      employee.name.first.indexOf(inputField) > -1 || employee.name.last.indexOf(inputField) > -1);
     }
-  // If the "if statement" evaluates to false, then invoke the 
-  //displayEmployees function and pass the stored JSON data to begin execution of the displayEmployees function 
-  //To display this employee data to the page.
+
+  /* If the "if statement" evaluates to false, then invoke the displayEmployees function and 
+     pass the stored JSON data to begin execution of the displayEmployees function To display 
+     this employee data to the page. */
      displayEmployees(filteredEmployees); 
   }
-// get the h2 data from modal card and returns the value to be used later in both arrow click event listeners
+
+/*get the h2 data from modal card and returns the value to be used later in both arrow 
+  click event listeners*/
   function getModalCard(){
     return document.getElementById("modalCard");
   }
-// gets the current index of the modal and returns the value to be used later in both arrow click event listeners
+
+/*gets the current index of the modal and returns the value to be used later in both arrow 
+  click event listeners */
   function getCurrentIndex(modalCard){
     return modalCard.getAttribute('data-index');
   }
   
-  // ------------------------------------------
-  //  EVENT LISTENERS
-  // ------------------------------------------
-// when user clicks on an employee card, it grabs the the closet employee card and grads the number value from the
-// The listener also passes a the indexed number value to the showModal function
+// --------------------------------------------------------------------//
+//  EVENT LISTENERS
+// --------------------------------------------------------------------//
+
+/* when user clicks on an employee card, it grabs the the closet employee card and grabs the 
+   number value from the listener also passes a the indexed number value to the showModal 
+   function */
   gridContainer.addEventListener("click", (event) =>{
     if(event.target !== gridContainer){
       const card = event.target.closest(".card");
@@ -154,7 +180,9 @@ function filterEmployee(){
       showModal(index);
     }
   });
-// when user clicks on left arrow it goes to the prev indexed person in the employee list and loops back to the start
+
+/* when user clicks on left arrow it goes to the prev indexed person in the employee list and 
+   loops back to the start */
   leftArrow.addEventListener("click", (event)=>{
     let card = getModalCard();
     let currentIndex = +getCurrentIndex(card);
@@ -165,7 +193,9 @@ function filterEmployee(){
      card.setAttribute('data-index', currentIndex)
      showModal(currentIndex);
   });
-// when user clicks on right arrow it goes to the next indexed person in the employee list and starts back around again
+
+/* when user clicks on right arrow it goes to the next indexed person in the employee list and
+   starts back around again*/
   rightArrow.addEventListener("click", (event)=>{
     let card = getModalCard();
     let currentIndex = +getCurrentIndex(card);
@@ -176,12 +206,14 @@ function filterEmployee(){
      card.setAttribute('data-index', currentIndex)
      showModal(currentIndex);
   });
+
 // when user clicks the close button it hides the modal/closes the modal compeletly
   closeModal.addEventListener("click", ()=>{
     overlay.classList.add("hidden");
   });
-//When user keys in a value (letter) it filters through the employees and display the employees 
-//that have with that value in their first & last name
+
+/* When user keys in a value (letter) it filters through the employees and display 
+   the employees that have with that value in their first & last name */
   const searchBar = document.getElementById("searchBar");
   searchBar.addEventListener("keyup", filterEmployee);
   
