65863b6956a0599c6d722dfbb9a37974acce1ce6
Brian Johnson <johnson8727@hotmail.com>
2020-03-05 20:52:22 -0500

    updated randomUser.js

---
+++ js/randomUser.js
@@ -0,0 +1,16 @@
+class RandomUserAPI{
+  constructor(){
+    this.url = `https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US`;
+  }
+// Fetch Random User JSON object from API
+async getRandomUser(){
+  const response = await fetch (this.url);
+  const responseData = await response.json();
+  return responseData;
+}
+}
+
+// --------------------------------------------------------------------//
+//  EXPORT THIS CLASS INTO MAIN.JS
+// --------------------------------------------------------------------//
+export const randomUser = new RandomUserAPI();
